69384d5c8d937d0e668c0753e4b517af25175dc7
Vishal V D <157345717+Vishal-V-D@users.noreply.github.com>
2025-09-28 19:27:51 +0530

    Updated apiKeyCheck middleware to validate against process.env.API_KEY

--- course-service/src/middleware/apiKeyCheck.ts
+++ course-service/src/middleware/apiKeyCheck.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export function apiKeyCheck(req: Request, res: Response, next: NextFunction) {
   const apiKey = req.query.apiKey;
-  if (apiKey === "validKey") {
+
+  if (apiKey === process.env.API_KEY) {
     next();
   } else {
     return res.status(403).json({ message: "Forbidden: Invalid API Key" });
